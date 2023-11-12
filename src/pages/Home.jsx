e65ce852db9a620e02abf74b2a239428cfa2518a
khusnulll <khusnull132@gmail.com>
2023-11-13 00:43:11 +0700

    menambahkan smooth scrolling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 
 import Header from "../parts/Header.jsx";
-import Hero from "../parts/Hero.jsx";
+import Hero from "../parts/Homepage/Hero.jsx";
 import BrowseRoom from "../parts/Homepage/BrowseRoom.jsx";
 import JustArrived from "../parts/Homepage/JustArrived.jsx";
 import Clients from "../parts/Clients.jsx";
 import SiteMap from "../parts/Homepage/SiteMap.jsx";
 import Footer from "../parts/Footer.jsx";
 
+import useScrollAnchor from "../helpers/hooks/useScrollAnchor.jsx";
+
 function Home() {
+  useScrollAnchor();
   return (
     <div>
       <Header theme={"white"} position={"absolute"} />
--- src/parts/Footer.jsx
+++ src/parts/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="flex text-center px-4 py-8 justify-center">
-      <p className="text-sm">Copyright 2021 • All Rights Reserved LuxSpace by Khusnul Khotimah</p>
+      <p className="text-sm">Copyright 2023 • All Rights Reserved LuxSpace by Khusnul Khotimah</p>
     </footer>
   );
 };
