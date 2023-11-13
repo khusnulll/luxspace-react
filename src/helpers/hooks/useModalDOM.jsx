0250ea0fa7a33894124da1593cb30e488fbbe121
khusnulll <khusnull132@gmail.com>
2023-11-13 20:37:47 +0700

    menambahkan browseroom

---
+++ src/helpers/hooks/useModalDOM.jsx
@@ -0,0 +1,14 @@
+import { useLayoutEffect } from "react";
+import { addClass } from "helpers/format/classNameModifier.js";
+
+const useModalDOM = () => {
+  useLayoutEffect(() => {
+    first;
+
+    return () => {
+      second;
+    };
+  }, [third]);
+};
+
+export default useModalDOM;
--- src/parts/Homepage/BrowseRoom.jsx
+++ src/parts/Homepage/BrowseRoom.jsx
@@ -1,6 +1,10 @@
 import React from "react";
+import { useAsync } from "react-async";
 
 const BrowseRoom = () => {
+  const { data, status, error, run } = useAsync({ data: { username: "" } });
+
+  console.log(data, status, error);
   return (
     <section className="flex bg-gray-100 py-16 px-4" id="browse-the-room">
       <div className="container mx-auto">
