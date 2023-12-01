6f60f023521ada9f92cce6c154b56263c400d24e
khusnulll <khusnull132@gmail.com>
2023-12-01 13:23:57 +0700

    memperbaiki gambar logo pada sitemap

--- src/parts/Homepage/SiteMap.jsx
+++ src/parts/Homepage/SiteMap.jsx
@@ -7,7 +7,7 @@ const SiteMap = () => {
     <section className="sitemap">
       <div className="border-b border-gray-200 py-12 mt-16 px-4">
         <div className="flex justify-center mb-8">
-          <img src="../.././public/assets/images/content/logo.png" alt="Luxspace | Fulfill your house with beautiful furniture" />
+          <img src="../../assets/images/content/logo.png" alt="Luxspace | Fulfill your house with beautiful furniture" />
         </div>
         <aside className="container mx-auto">
           <div className="flex flex-wrap -mx-4 justify-center">
