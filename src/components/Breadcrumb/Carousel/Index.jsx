edcf3ed784ab5e793e19942e27521ee53dacc493
Khusnul Khotimah <khusnull132@gmail.com>
2024-03-08 04:17:16 -0800

    menambahkan carousel

---
+++ src/components/Breadcrumb/Carousel/Index.jsx
@@ -0,0 +1,56 @@
+import React, {useRef, useState, useCallback, useLayoutEffect} from 'react'
+
+const Carousel = ({children, refContainer}) => {
+  const refDragHandler = useRef(null)
+  const [index, setIndex] = useState(0)
+
+  const treshold = 100
+  const itemToShow = window.innerWidth < 767 ? 1 : 4
+  const DIRECTION_LEFT = "DIRECTION_LEFT"
+  const DIRECTION_RIGHT = "DIRECTION_RIGHT"
+  
+  const posInitial = useRef()
+  const posX1 = useRef()
+  const posX2 = useRef()
+  const posFinal = useRef()
+  const isAllowShift = useRef(true)
+  const cards = useRef()
+
+  const onDragMove = useCallback(
+    (e) => {
+      e = e || window.Event
+      console.log(e)
+    },
+    [],
+  )
+
+  const onDragEnd = useCallback(
+    (e) => {
+      e = e || window.Event
+      console.log(e)
+    },
+    [],
+  )
+
+  const onDragStart = useCallback(
+    (e) => {
+      e = e || window.Event
+      console.log(e)
+    },
+    [],
+  )
+
+  useLayoutEffect(() => {
+    first
+  
+    return () => {
+      second
+    };
+  }, [third])
+
+  return (
+    <div ref={refDragHandler} className="flex -mx-4 flex-row relative">{children}</div>
+  )
+}
+
+export default Carousel
